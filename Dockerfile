97b769991cf8430a235ce384c432b4c20ed66204
Max Findel <maxfindel@gmail.com>
2017-03-29 11:02:36 +0200

    exposing 8080 for nginx

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,6 @@ WORKDIR /app
 RUN pip install --upgrade pip
 # Install requirements
 RUN pip install -r requirements.txt
+
+EXPOSE 8080
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def new():
     return redirect(url_for('todo'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
