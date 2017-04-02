42ebcf61dd70c6c1be7ca06267444900e10e3ad1
Gerr <german@arriendas.cl>
2017-04-02 02:39:10 -0300

    transripciones

--- speech.py
+++ speech.py
@@ -1,5 +1,7 @@
 import sys
 reload(sys)
+# play -r 8000 -b 16 -c 1 -e signed [226612700]_[226612700]_[30-03-2017]_[16-48-00].raw
+
 sys.setdefaultencoding('utf-8')
 import time
 import io
@@ -9,9 +11,9 @@ import os
 from google.cloud import speech
 hints = ['pantalla', 'iphone', '119', '69','bateria']
 client = speech.Client()
-sample = client.sample(source_uri='gs://neemfs/2.raw',
+sample = client.sample(source_uri='gs://neemfs/[+56998417174]_[+56998417174]_[31-03-2017]_[12-41-34].raw',
                         encoding=speech.Encoding.LINEAR16,
-                        sample_rate=16000)
+                        sample_rate=8000)
 operation = sample.async_recognize(language_code='es-CL',max_alternatives=2,speech_context=hints)
 
 retry_count = 100
